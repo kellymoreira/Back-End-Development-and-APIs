27e05875b91babcfc1690bb85c66df9300693509
Kelly Cristina Moreira <kelly.moreira0510@gmail.com>
2023-12-26 01:49:22 -0300

    Create 04-Serve Static Assets myApp.js

---
+++ Basic Node and Express/04-Serve Static Assets myApp.js
@@ -0,0 +1,12 @@
+let express = require("express");
+let app = express();
+
+console.log("Hello World");
+
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/views/index.html`);
+});
+
+app.use("/public", express.static(__dirname + "/public"));
+
+module.exports = app;
