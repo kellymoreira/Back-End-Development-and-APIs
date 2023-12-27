6b201de9f9282461a287df43641cce772323fc91
Kelly Cristina Moreira <kelly.moreira0510@gmail.com>
2023-12-27 14:08:15 -0300

    Update 02-Start a Working Express Server - myApp.js

--- Basic Node and Express/02-Start a Working Express Server - myApp.js
+++ Basic Node and Express/02-Start a Working Express Server - myApp.js
@@ -1,8 +1,10 @@
 let express = require("express");
 let app = express();
 
+// 1) Meet the Node Console.
 console.log("Hello World");
 
+// 2) Start a Working Express Server
 app.get("/", (req, res) => {
   res.send("Hello Express");
 });
