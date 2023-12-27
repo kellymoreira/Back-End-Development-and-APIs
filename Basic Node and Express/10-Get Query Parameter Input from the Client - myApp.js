f39119ca3433117e8c17d656e0b76eb5b26d1a52
Kelly Cristina Moreira <kelly.moreira0510@gmail.com>
2023-12-27 15:20:34 -0300

    Update 10-Get Query Parameter Input from the Client - myApp.js

--- Basic Node and Express/10-Get Query Parameter Input from the Client - myApp.js
+++ Basic Node and Express/10-Get Query Parameter Input from the Client - myApp.js
@@ -1,21 +1,38 @@
 let express = require("express");
 let app = express();
 
+// 7) Implement a Root-Level Request Logger Middleware
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
 });
 
+// 1) Meet the Node Console.
 console.log("Hello World");
 
+/*************************************
+// 2) Start a Working Express Server
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+*************************************/
+
+// 3) Serve an HTML File
 app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/views/index.html`);
 });
 
+// 4) Serve Static Assets
 app.use("/public", express.static(__dirname + "/public"));
 
-module.exports = app;
+/****************************************
+// 5) Serve JSON on a Specific Route
+app.get("/json", (req, res) => {
+  res.json({ message: "Hello json" });
+});
+****************************************/
 
+// 6) Use the .env File to Configure the App
 app.get("/json", (req, res) => {
   let message = "Hello json";
 
@@ -26,6 +43,10 @@ app.get("/json", (req, res) => {
   res.json({ message });
 });
 
+// 7) Implement a Root-Level Request Logger Middleware
+/* IMPLEMENT IT BEFORE ALL THE ROUTES */
+
+// 8) Chain Middleware to Create a Time Server
 app.get(
   "/now",
   (req, res, next) => {
@@ -38,10 +59,14 @@ app.get(
   },
 );
 
+// 9) Get Input from Client - Route Parameters
 app.get("/:word/echo", (req, res) => {
   res.json({ echo: req.params.word });
 });
 
+// 10) Get Input from Client - Query Parameters
 app.route("/name").get((req, res) => {
   res.json({ name: `${req.query.first} ${req.query.last}` });
 });
+
+module.exports = app;
