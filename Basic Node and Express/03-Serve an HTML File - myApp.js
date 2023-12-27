0f7f388e69f8cd7cbb01bb57574074066d245b2a
Kelly Cristina Moreira <kelly.moreira0510@gmail.com>
2023-12-27 14:18:10 -0300

    Update 03-Serve an HTML File - myApp.js

--- Basic Node and Express/03-Serve an HTML File - myApp.js
+++ Basic Node and Express/03-Serve an HTML File - myApp.js
@@ -1,8 +1,17 @@
 let express = require("express");
 let app = express();
 
+// 1) Meet the Node Console.
 console.log("Hello World");
 
+/*************************************
+// 2) Start a Working Express Server
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+*************************************/
+
+// 3) Serve an HTML File
 app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/views/index.html`);
 });
