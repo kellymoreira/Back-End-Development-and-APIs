0ce2af591c054ca9969661797ae79b8caf00d074
Kelly Cristina Moreira <kelly.moreira0510@gmail.com>
2023-12-27 13:05:08 -0300

    Update 11-Use body-parser to Parse POST Requests - myApp.js

--- Basic Node and Express/11-Use body-parser to Parse POST Requests - myApp.js
+++ Basic Node and Express/11-Use body-parser to Parse POST Requests - myApp.js
@@ -1,1 +1,51 @@
-.
+let express = require("express");
+let bodyParser = require("body-parser");
+let app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
+console.log("Hello World");
+
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/views/index.html`);
+});
+
+app.use("/public", express.static(__dirname + "/public"));
+
+module.exports = app;
+
+app.get("/json", (req, res) => {
+  let message = "Hello json";
+
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase();
+  }
+
+  res.json({ message });
+});
+
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+
+  (req, res) => {
+    res.json({ time: req.time });
+  },
+);
+
+app.get("/:word/echo", (req, res) => {
+  res.json({ echo: req.params.word });
+});
+
+app.route("/name").get((req, res) => {
+  res.json({ name: `${req.query.first} ${req.query.last}` });
+});
