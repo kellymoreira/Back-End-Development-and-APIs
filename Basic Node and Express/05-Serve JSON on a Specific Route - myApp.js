a5cf2c46490f4e24d8a2689f31a1873257fadcc7
Kelly Cristina Moreira <kelly.moreira0510@gmail.com>
2023-12-27 14:24:50 -0300

    Update 05-Serve JSON on a Specific Route - myApp.js

--- Basic Node and Express/05-Serve JSON on a Specific Route - myApp.js
+++ Basic Node and Express/05-Serve JSON on a Specific Route - myApp.js
@@ -1,16 +1,29 @@
 let express = require("express");
 let app = express();
 
+// 1) Meet the Node Console.
 console.log("Hello World");
 
+/*************************************
+// 2) Start a Working Express Server
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+*************************************/
+
+// 3) Serve an HTML File
 app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/views/index.html`);
 });
 
+// 4) Serve Static Assets
 app.use("/public", express.static(__dirname + "/public"));
 
-module.exports = app;
-
+// 5) Serve JSON on a Specific Route
 app.get("/json", (req, res) => {
   res.json({ message: "Hello json" });
 });
+
+module.exports = app;
+
+
