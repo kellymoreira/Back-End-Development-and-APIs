ccf046c18b9a67ef01a33ae27e12f56e1a5003de
Kelly Cristina Moreira <kelly.moreira0510@gmail.com>
2023-12-27 14:21:34 -0300

    Update 04-Serve Static Assets - myApp.js

--- Basic Node and Express/04-Serve Static Assets - myApp.js
+++ Basic Node and Express/04-Serve Static Assets - myApp.js
@@ -1,12 +1,22 @@
 let express = require("express");
 let app = express();
 
+// 1) Meet the Node Console.
 console.log("Hello World");
 
+/*************************************
+// 2) Start a Working Express Server
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+*************************************/
+
+// 3) Serve an HTML File
 app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/views/index.html`);
 });
 
+// 4) Serve Static Assets
 app.use("/public", express.static(__dirname + "/public"));
 
 module.exports = app;
