ae5212f43e3757edd5824f8701048c8846a9425d
Kelly Cristina Moreira <kelly.moreira0510@gmail.com>
2023-12-27 15:35:48 -0300

    Update 11-Use body-parser to Parse POST Requests - myApp.js

--- Basic Node and Express/11-Use body-parser to Parse POST Requests - myApp.js
+++ Basic Node and Express/11-Use body-parser to Parse POST Requests - myApp.js
@@ -2,24 +2,42 @@ let express = require("express");
 let bodyParser = require("body-parser");
 let app = express();
 
+// 11) Use body-parser to Parse POST Requests
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// 7) Implement a Root-Level Request Logger Middleware
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
 });
 
+// 1) Meet the Node Console.
 console.log("Hello World");
 
+/*************************************
+// 2) Start a Working Express Server
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+*************************************/
+
+// 3) Serve an HTML File
 app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/views/index.html`);
 });
 
+// 4) Serve Static Assets
 app.use("/public", express.static(__dirname + "/public"));
 
-module.exports = app;
+/****************************************
+// 5) Serve JSON on a Specific Route
+app.get("/json", (req, res) => {
+  res.json({ message: "Hello json" });
+});
+****************************************/
 
+// 6) Use the .env File to Configure the App
 app.get("/json", (req, res) => {
   let message = "Hello json";
 
@@ -30,6 +48,10 @@ app.get("/json", (req, res) => {
   res.json({ message });
 });
 
+// 7) Implement a Root-Level Request Logger Middleware
+/* IMPLEMENT IT BEFORE ALL THE ROUTES */
+
+// 8) Chain Middleware to Create a Time Server
 app.get(
   "/now",
   (req, res, next) => {
@@ -42,10 +64,18 @@ app.get(
   },
 );
 
+// 9) Get Input from Client - Route Parameters
 app.get("/:word/echo", (req, res) => {
   res.json({ echo: req.params.word });
 });
 
+// 10) Get Input from Client - Query Parameters
 app.route("/name").get((req, res) => {
   res.json({ name: `${req.query.first} ${req.query.last}` });
 });
+
+// 11) Use body-parser to Parse POST Requests
+/* IMPLEMENT IT BEFORE ALL THE ROUTES */
+
+module.exports = app;
+
