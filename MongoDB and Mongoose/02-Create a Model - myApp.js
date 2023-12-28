f587cda55537abebc9dacd7b7da1cc1506857ae3
Kelly Cristina Moreira <kelly.moreira0510@gmail.com>
2023-12-28 18:03:42 -0300

    Update 02-Create a Model - myApp.js

--- MongoDB and Mongoose/02-Create a Model - myApp.js
+++ MongoDB and Mongoose/02-Create a Model - myApp.js
@@ -1,1 +1,90 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
 
+// 1) Install and Set Up Mongoose
+/** CONNECT TO THE MONGODB ATLAS DATABASE USING THE PROVIDED URI **/
+/* MONGO_URI=mongodb+srv://Username:Password@ServerOrCluster/DesiredDatabaseName?AdditionalConnectionOptions */
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// 2) Create a "Person" Model
+/** ASSIGN MONGOOSE SCHEMA TO A VARIABLE **/
+const Schema = mongoose.Schema;
+
+/** CREATE PERSON SCHEMA **/
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: { type: [String], required: true },
+});
+
+/** CREATE PERSON MODEL FROM THE SCHEMA **/
+const Person = mongoose.model("Person", personSchema);
+
+const createAndSavePerson = (done) => {
+  done(null /*, data*/);
+};
+
+const createManyPeople = (arrayOfPeople, done) => {
+  done(null /*, data*/);
+};
+
+const findPeopleByName = (personName, done) => {
+  done(null /*, data*/);
+};
+
+const findOneByFood = (food, done) => {
+  done(null /*, data*/);
+};
+
+const findPersonById = (personId, done) => {
+  done(null /*, data*/);
+};
+
+const findEditThenSave = (personId, done) => {
+  const foodToAdd = "hamburger";
+
+  done(null /*, data*/);
+};
+
+const findAndUpdate = (personName, done) => {
+  const ageToSet = 20;
+
+  done(null /*, data*/);
+};
+
+const removeById = (personId, done) => {
+  done(null /*, data*/);
+};
+
+const removeManyPeople = (done) => {
+  const nameToRemove = "Mary";
+
+  done(null /*, data*/);
+};
+
+const queryChain = (done) => {
+  const foodToSearch = "burrito";
+
+  done(null /*, data*/);
+};
+
+/** **Well Done !!**
+/* You completed these challenges, let's go celebrate !
+ */
+
+//----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
+
+exports.PersonModel = Person;
+exports.createAndSavePerson = createAndSavePerson;
+exports.findPeopleByName = findPeopleByName;
+exports.findOneByFood = findOneByFood;
+exports.findPersonById = findPersonById;
+exports.findEditThenSave = findEditThenSave;
+exports.findAndUpdate = findAndUpdate;
+exports.createManyPeople = createManyPeople;
+exports.removeById = removeById;
+exports.removeManyPeople = removeManyPeople;
+exports.queryChain = queryChain;
